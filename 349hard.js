28ee91b5290bee57ce512569bc51bea02e4780ee
ck <51444578+ChrisKalms@users.noreply.github.com>
2020-06-23 19:40:03 +0800

    first

--- 349hard.js
+++ 349hard.js
@@ -1,4 +1,23 @@
+// Soundex is an algorithm used to categorize phonetically, such that two names that sound alike but are spelled differently have the same representation.
+// Soundex maps every name to a string consisting of one letter and three numbers, like M460.
+// One version of the algorithm is as follows:
+// 1.	Remove consecutive consonants with the same sound (for example, change ck -> c).
+// 2.	Keep the first letter. The remaining steps only apply to the rest of the string.
+// 3.	Remove all vowels, including y, w, and h.
+// 4.	Replace all consonants with the following digits:
+// o	b, f, p, v → 1
+// o	c, g, j, k, q, s, x, z → 2
+// o	d, t → 3
+// o	l → 4
+// o	m, n → 5
+// o	r → 6
+// 5.	If you don't have three numbers yet, append zeros until you do. Keep the first three numbers.
+// Using this scheme, Jackson and Jaxen both map to J250.
+// Implement Soundex.
 
+
+
+//TODO Remove Consecutive consonants with the same sound
 let str = "Jackson";
 const soundex = (str) => {
   let arr = str.split("");
@@ -10,7 +29,7 @@ const soundex = (str) => {
     ["d", "t"],
     ["l"],
     ["m", "n"],
-    ["r"]
+    ["r"],
   ];
 
   for (let i = 0; i < arr.length; i++) {
@@ -33,26 +52,25 @@ const soundex = (str) => {
       if (!sentinal) {
         let sentinal2 = false;
         for (let j = 0; j < consonants.length && !sentinal2; j++) {
-          //loop through outer array 
+          //loop through outer array
           for (let k = 0; k < consonants[j].length; k++) {
             //loop through inner array
             let cons = consonants[j][k];
             console.log(cons);
             //console.log(cons);
-            if (char === cons){
-                out.push(j + 1); // j+1 will be the digit we need                
-                sentinal2 = true;
-                break;
+            if (char === cons) {
+              out.push(j + 1); // j+1 will be the digit we need
+              sentinal2 = true;
+              break;
             }
           }
         }
       }
     } else {
-        out.push(arr[0]);
+      out.push(arr[0]);
     }
   }
   console.log(out);
 };
-
 
 soundex(str);
--- prob31.js
+++ prob31.js
@@ -1,101 +1,90 @@
- 
-        var listeners = [1,5,11,20,50,100];
-        var towers = [4, 8, 15,19,25];
+//You are the technical director of WSPT radio, serving listeners nationwide. For simplicity's sake we can consider each listener to live along a horizontal line stretching from 0 (west) to 1000 (east).
+//Given a list of N listeners, and a list of M radio towers, each placed at various locations along this line, determine what the minimum broadcast range would have to be in order for each listener's home to be covered.
+//For example, suppose listeners = [1, 5, 11, 20], and towers = [4, 8, 15]. In this case the minimum range would be 5, since that would be required for the tower at position 15 to reach the listener at position 20.
 
-        console.log(solve(listeners,towers));
-        console.log(solveFasterIfOrdered(listeners,towers));
-        console.log(solveEvenFasterIfOrdered(listeners,towers));
+var listeners = [1, 5, 11, 20, 50, 100];
+var towers = [4, 8, 15, 19, 25];
 
-        function solve (l, t) {
-            var count = 0;
-            var closest = [];
-            for(var i = 0; i < l.length; i++){
-                for(var j = 0; j < t.length; j++){
-                    if(j === 0){
-                        closest[i] = l[i] - t[j];
-                        if (closest[i] < 0) {
-                            closest[i] *= -1;
-                        }                        
-                    } else {
-                        var distance = l[i] - t[j];
-                        if ( distance < 0 ) distance *= -1;
-                        if ( distance < closest[i]) closest[i] = distance;
-                    } 
-                    count++;                   
-                }
-            }
-            console.log(count);
-            return Math.max(...closest);
+console.log(solve(listeners, towers));
+console.log(solveFasterIfOrdered(listeners, towers));
+console.log(solveEvenFasterIfOrdered(listeners, towers));
 
-            /* Solves the furthest not the least :|
-            var distance = 0;
-            for(var i = 0; i < l.length; i++) { //listeners
-                for(var j = 0; j < t.length; j++){ //towers
-                    var x = l[i] - t[j];
-                    if( x < 0 ) {
-                        x = x * -1;
-                    }
-                    if(x > distance){
-                        distance = x;
-                    }
-                }
-            }
-            console.log(distance);
-            */
+function solve(l, t) {
+  var count = 0;
+  var closest = [];
+  for (var i = 0; i < l.length; i++) {
+    for (var j = 0; j < t.length; j++) {
+      if (j === 0) {
+        closest[i] = l[i] - t[j];
+        if (closest[i] < 0) {
+          closest[i] *= -1;
         }
+      } else {
+        var distance = l[i] - t[j];
+        if (distance < 0) distance *= -1;
+        if (distance < closest[i]) closest[i] = distance;
+      }
+      count++;
+    }
+  }
+  console.log(count);
+  return Math.max(...closest);
+}
 
-        function solveFasterIfOrdered(l,t){            
-            //[1,5,11,20]
-            //[4,8,15]
-            var closest = [];            
-            var count = 0;
+function solveFasterIfOrdered(l, t) {
+  //[1,5,11,20]
+  //[4,8,15]
+  var closest = [];
+  var count = 0;
 
-            for(var i = 0; i < l.length; i++){
-                for(var j = 0; j < t.length; j++){
-                    if(j === 0){
-                        closest[i] = l[i] - t[j];                        
-                        if (closest[i] < 0) {
-                            closest[i] *= -1;
-                        }                          
-                    } else {
-                        var distance = l[i] - t[j];
-                        if ( distance < 0 ) distance *= -1;
-                        if ( distance < closest[i]) closest[i] = distance;
-                        else {break}
-                    }   
-                    count++;                
-                }
-            }
-            console.log(count);
-            return Math.max(...closest);
+  for (var i = 0; i < l.length; i++) {
+    for (var j = 0; j < t.length; j++) {
+      if (j === 0) {
+        closest[i] = l[i] - t[j];
+        if (closest[i] < 0) {
+          closest[i] *= -1;
         }
- 
-        function solveEvenFasterIfOrdered(l,t){            
-            //[1,5,11,20]
-            //[4,8,15]
-            var closest = [];            
-            var count = 0;
-            var lastTower = 0;
-            for(var i = 0; i < l.length; i++){
-                for(var j = lastTower; j < t.length; j++){
-                    if(j === lastTower){
-                        closest[i] = l[i] - t[j];                        
-                        if (closest[i] < 0) {
-                            closest[i] *= -1;
-                        }                          
-                    } else {
-                        var distance = l[i] - t[j];
-                        if ( distance < 0 ) distance *= -1;
-                        if ( distance < closest[i]) {
-                            closest[i] = distance;
-                            lastTower = j;
-                        } else {                            
-                            break;
-                        }
-                    }   
-                    count++;                
-                }
-            }
-            console.log(count);
-            return Math.max(...closest);
+      } else {
+        var distance = l[i] - t[j];
+        if (distance < 0) distance *= -1;
+        if (distance < closest[i]) closest[i] = distance;
+        else {
+          break;
         }
+      }
+      count++;
+    }
+  }
+  console.log(count);
+  return Math.max(...closest);
+}
+
+function solveEvenFasterIfOrdered(l, t) {
+  //[1,5,11,20]
+  //[4,8,15]
+  var closest = [];
+  var count = 0;
+  var lastTower = 0;
+  for (var i = 0; i < l.length; i++) {
+    for (var j = lastTower; j < t.length; j++) {
+      if (j === lastTower) {
+        closest[i] = l[i] - t[j];
+        if (closest[i] < 0) {
+          closest[i] *= -1;
+        }
+      } else {
+        var distance = l[i] - t[j];
+        if (distance < 0) distance *= -1;
+        if (distance < closest[i]) {
+          closest[i] = distance;
+          lastTower = j;
+        } else {
+          break;
+        }
+      }
+      count++;
+    }
+  }
+  console.log(count);
+  return Math.max(...closest);
+}
